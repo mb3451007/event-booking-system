8f7afb10e9a8e62e7343b6bd43aa3f9c4537e322
saadasif04 <asifnawazmalik75@gmai.com@example.com>
2025-06-04 21:18:17 +0500

    Changed env for live

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  // apiUrl: 'http://213.130.147.195:3000',
-  apiUrl: 'http://localhost:3000',
+  production: true,
+  apiUrl: 'http://213.130.147.195:3000',
+  // apiUrl: 'http://localhost:3000',
   // apiUrl: ' https://0108-154-192-1-66.ngrok-free.app',
   // apiUrl: 'https://adlerpalast.de:3000',
 };
